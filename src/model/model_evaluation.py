d7fd8a97c905e51d604cee55956357269ecd8097
Santosh Chapagain <santoahsharam98@gmail.com>
2026-08-18 23:26:07 +0545

    CICD-6

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -14,27 +14,27 @@ from src.logger import logging
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("DAGSHUB_TOKEN")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "santoshchapagain"
-# repo_name = "Sentiment_Analysis_Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "santoshchapagain"
+repo_name = "Sentiment_Analysis_Project"
 
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri(
-    'https://dagshub.com/Santosh-Chapagain/Movie_Sentiment_Analysis.mlflow')
-dagshub.init(repo_owner='Santosh-Chapagain',
-             repo_name='Movie_Sentiment_Analysis', mlflow=True)
+# mlflow.set_tracking_uri(
+#     'https://dagshub.com/Santosh-Chapagain/Movie_Sentiment_Analysis.mlflow')
+# dagshub.init(repo_owner='Santosh-Chapagain',
+#              repo_name='Movie_Sentiment_Analysis', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
